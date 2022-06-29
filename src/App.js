c6fd2a27de96778ca8325e21761a455245d60142
kaushal aggarwal <kaushal.aggarwal@kaushal-ltmcpku.internal.salesforce.com>
2022-06-29 10:12:50 +0530

    a

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         formInput={ formInput }
         isActive = {isActive}
         />
-        <Recorder
+        <Demo
         updateIsActive = {updateIsActive}
         />
       </div>
